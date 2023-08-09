5e0808abe637313b9e164cb753045a62fa2e6a2f
road2Coder <djaffre.prog@gmail.com>
2023-08-09 18:10:45 +0200

    Modification

--- dist/main.js
+++ dist/main.js
@@ -1,22 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Camps = void 0;
+import gsap from "gsap";
+const character = document.querySelector(".character");
+// const desc = document.querySelector("#desc") as HTMLDivElement;
+gsap.from(character, { opacity: 0, x: 85, duration: 0.7 });
 // To parse this data:
 // @ts-ignore
-const charact_json_1 = __importDefault(require("../dist/data/charact.json"));
+import charact from "../dist/data/charact.json" assert { type: "json" };
 //
 //   const welcome = Convert.toWelcome(json);
 //
 // These functions will throw an error if the JSON doesn't
 // match the expected interface, even if the JSON is valid.
-const characters = charact_json_1.default;
-console.log(characters.perso[0]);
-var Camps;
+const characters = charact;
+export var Camps;
 (function (Camps) {
     Camps["Etudiant"] = "etudiant";
     Camps["Heros"] = "heros";
     Camps["Vilain"] = "vilain";
-})(Camps || (exports.Camps = Camps = {}));
+})(Camps || (Camps = {}));
