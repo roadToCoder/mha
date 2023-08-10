5463f58bf226aaeff0c4be5f307a2bf16298d70b
road2Coder <djaffre.prog@gmail.com>
2023-08-10 11:48:46 +0200

    Modification

--- dist/main.js
+++ dist/main.js
@@ -1,19 +1,30 @@
-import gsap from "gsap";
-const character = document.querySelector(".character");
-// const desc = document.querySelector("#desc") as HTMLDivElement;
-gsap.from(character, { opacity: 0, x: 85, duration: 0.7 });
+import gsap from "../node_modules/gsap/index.js";
+const imgCharacter = document.querySelector(".imgCharacter");
+gsap.from(imgCharacter, { opacity: 0, x: 85, duration: 0.7 });
+const displayName = document.querySelector("#displayName");
+const prevButton = document.getElementById('#prevButton');
+const nextButton = document.getElementById('#nextButton');
+let currentCharacterIndex = 0;
+let charactersData = []; // Data fetched from JSON
 // To parse this data:
 // @ts-ignore
 import charact from "../dist/data/charact.json" assert { type: "json" };
-//
-//   const welcome = Convert.toWelcome(json);
-//
-// These functions will throw an error if the JSON doesn't
-// match the expected interface, even if the JSON is valid.
 const characters = charact;
+// console.log(characters.perso[0].age);
+displayName.innerHTML = `<div>${characters.perso[0].prenom} ${characters.perso[0].nom}</div>`;
 export var Camps;
 (function (Camps) {
     Camps["Etudiant"] = "etudiant";
     Camps["Heros"] = "heros";
     Camps["Vilain"] = "vilain";
 })(Camps || (Camps = {}));
+prevButton.addEventListener('click', () => {
+    currentCharacterIndex = (currentCharacterIndex - 1 + charactersData.length) % charactersData.length;
+    updateCharacterContent(currentCharacterIndex);
+});
+nextButton.addEventListener('click', () => {
+    currentCharacterIndex = (currentCharacterIndex + 1) % charactersData.length;
+    updateCharacterContent(currentCharacterIndex);
+});
+function updateCharacterContent(index) {
+}
