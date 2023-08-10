5bc7d6655e30366d5170ddea58cb083689881c3d
road2Coder <djaffre.prog@gmail.com>
2023-08-10 16:43:04 +0200

    Modification

--- dist/main.js
+++ dist/main.js
@@ -18,17 +18,8 @@ export var Camps;
 const errorInformation = document.querySelector("#errorInformation");
 const displayInfos = document.querySelector("#displayInfos");
 const displayImg = document.querySelector("#displayImg");
-// import charact from "../dist/data/json/charact.json" assert { type: "json" };
-let characters = [];
-// async function fetchCharacterData() {
-//   const response = await fetch('./dist/data/json/charact.json');
-//   const data = await response.json();
-//   displayInfos.innerHTML= `<div>${data.perso[0].prenom} ${data.perso[0].nom}</div>`
-//   characters = data;
-//   return data;
-// }
-// fetchCharacterData()
-// console.log(characters[0].alias);
+const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
 function getCharacters() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -38,7 +29,16 @@ function getCharacters() {
             }
             const allCharacters = yield response.json();
             console.log(allCharacters);
-            displaysInfosCharacter(allCharacters);
+            let currentCharacterIndex = 0;
+            displayCharacter(allCharacters, currentCharacterIndex);
+            nextButton.addEventListener("click", () => {
+                currentCharacterIndex = (currentCharacterIndex + 1) % allCharacters.character.length;
+                displayCharacter(allCharacters, currentCharacterIndex);
+            });
+            prevButton.addEventListener('click', () => {
+                currentCharacterIndex = (currentCharacterIndex - 1 + allCharacters.character.length) % allCharacters.character.length;
+                displayCharacter(allCharacters, currentCharacterIndex);
+            });
         }
         // Gestion des erreurs
         catch (error) {
@@ -51,24 +51,23 @@ function getCharacters() {
         }
     });
 }
-function displaysInfosCharacter(characters) {
-    console.log(characters.character[0]);
+getCharacters();
+function displayCharacter(characters, index) {
     let txt = "";
-    txt += `<div>${characters.character[0].prenom} ${characters.character[0].nom}</div>`;
-    txt += `<div class="pr-3 pl-3">Surnom : ${characters.character[0].alias}</div>`;
-    txt += `<div class="pr-3 pl-3">Age : ${characters.character[0].age}</div>`;
-    txt += `<div class="pr-3 pl-3">Camps : ${characters.character[0].camps}</div>`;
-    if (characters.character[0].pro || characters.character[0].rang !== null) {
-        txt += `<div class="pr-3 pl-3">Rang : ${characters.character[0].rang}</div>`;
+    txt += `<div>${characters.character[index].prenom} ${characters.character[index].nom}</div>`;
+    txt += `<div class="pr-3 pl-3">Surnom : ${characters.character[index].alias}</div>`;
+    txt += `<div class="pr-3 pl-3">Age : ${characters.character[index].age}</div>`;
+    txt += `<div class="pr-3 pl-3">Camps : ${characters.character[index].camps}</div>`;
+    if (characters.character[index].pro || characters.character[index].rang !== null) {
+        txt += `<div class="pr-3 pl-3">Rang : ${characters.character[index].rang}</div>`;
     }
-    txt += `<div class="pr-3 pl-3">Pouvoir : ${characters.character[0].pouvoir}</div>`;
-    txt += `<div class="pr-3 pl-3">Desc power : ${characters.character[0].descPower}</div>`;
-    txt += `<div class="pr-3 pl-3">Histoire : ${characters.character[0].descHistory}</div>`;
+    txt += `<div class="pr-3 pl-3">Pouvoir : ${characters.character[index].pouvoir}</div>`;
+    txt += `<div class="pr-3 pl-3">Desc power : ${characters.character[index].descPower}</div>`;
+    txt += `<div class="pr-3 pl-3">Histoire : ${characters.character[index].descHistory}</div>`;
     displayInfos.innerHTML = txt;
     // // mettre ici un if pour indiquer quel paysage afficher en fonction du personnage
     displayImg.innerHTML = `<img src="./dist/img/res/Yuuei_Building.webp" class="university z-10 aspect-auto" alt="">`;
-    displayImg.innerHTML += `<img src="${characters.character[0].img}" class="${characters.character[0].supClass}">`;
+    displayImg.innerHTML += `<img src="${characters.character[index].img}" class="${characters.character[index].supClass}">`;
     const imgCharacter = document.querySelector(".imgCharacter");
     gsap.from(imgCharacter, { opacity: 0, x: 85, duration: 0.7 });
 }
-getCharacters();
